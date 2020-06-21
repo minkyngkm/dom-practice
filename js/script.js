87a03f21b244430e4a64128010e66d5e0993d0d8
minkyngkm <minkyngkm@gmail.com>
2020-06-21 17:01:12 +0900

    pratice getElementByTagName

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 window.addEventListener("load", function(){
+    var section2 = document.getElementById("section2");
+    var inputs =  section2.getElementsByTagName("input");
+
+    var txtX = inputs[0];
+    var txtY = inputs[1];
+    var btnAdd = inputs[2];
+    var txtSum = inputs[3];
+
+    btnAdd.onclick = function(){ 
+        var x = parseInt(txtX.value);
+        var y = parseInt(txtY.value);
+
+        txtSum.value = x+y; 
+    }
+})
+
+window.addEventListener("load", function(){
     var txtX = document.getElementById("txt-x");
     var txtY = document.getElementById("txt-y");
     var btnAdd = document.getElementById("btn-add");
